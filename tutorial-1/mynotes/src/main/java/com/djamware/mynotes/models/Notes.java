255842b4cfa6139ccdd3a99c630ca097ccd07b54
Ramos Garcia Luis Enrique (BCI/ESW17) <ral2be@bosch.com>
2023-12-19 15:52:55 +0100

    status
    status

--- tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/Notes.java
+++ tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/Notes.java
@@ -1,5 +1,73 @@
 package com.djamware.mynotes.models;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+
+@Entity
 public class Notes {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Date updated;
+
+	public Notes() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Notes(Long id, String title, String content, Date updated) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.updated = updated;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+    
+    
 
 }
