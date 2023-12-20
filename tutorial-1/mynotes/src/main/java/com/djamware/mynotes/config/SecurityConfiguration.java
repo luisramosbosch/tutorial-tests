1538137cbc400a1da0e96f4aa7d0b34ea10475e8
Ramos Garcia Luis Enrique (BCI/ESW17) <ral2be@bosch.com>
2023-12-20 15:06:20 +0100

    Update SecurityConfiguration.java
    status

---
+++ tutorial-1/mynotes/src/main/java/com/djamware/mynotes/config/SecurityConfiguration.java
@@ -0,0 +1,104 @@
+package com.djamware.mynotes.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.djamware.mynotes.services.CustomUserDetailsService;
+
+@Configuration
+public class SecurityConfiguration {
+	
+	@Autowired
+    CustomizeAuthenticationSuccessHandler customizeAuthenticationSuccessHandler;
+    
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+    }
+    
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+    	return new CustomUserDetailsService();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+    
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+         
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+     
+        return authProvider;
+    }
+    
+    
+    /*
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
+                .hasAnyAuthority("Admin", "Editor", "Salesperson")
+                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .loginPage("/login")
+                    .usernameParameter("email")
+                    .permitAll()
+                .and()
+                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
+                .and()
+                .logout().permitAll();
+ 
+        http.headers().frameOptions().sameOrigin();
+ 
+        return http.build();
+    }
+    */
+     
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http.cors().and().csrf().disable()
+          .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+          .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+          .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+          .antMatchers("/api/test/**").permitAll()
+          .anyRequest().authenticated();
+      
+      http.authenticationProvider(authenticationProvider());
+
+      http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+      
+      return http.build();
+    }
+    
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+    
+
+    
+}
