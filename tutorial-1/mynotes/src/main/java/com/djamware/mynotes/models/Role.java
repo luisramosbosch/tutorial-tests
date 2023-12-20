369e5ca635d86b473cb00d1cd642334548fb7822
Ramos Garcia Luis Enrique (BCI/ESW17) <ral2be@bosch.com>
2023-12-20 07:35:46 +0100

    status
    status

--- tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/Role.java
+++ tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/Role.java
@@ -1,5 +1,70 @@
 package com.djamware.mynotes.models;
 
-public class Role {
+import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "role")
+public class Role {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+    private String role;
+    
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	/**
+	 * @return the users
+	 */
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users the users to set
+	 */
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+    
+    
+	
 }
--- tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/User.java
+++ tutorial-1/mynotes/src/main/java/com/djamware/mynotes/models/User.java
@@ -1,5 +1,115 @@
 package com.djamware.mynotes.models;
 
-public class User {
+import java.util.Set;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+
+
+@Entity
+@Table(name = "user")
+
+
+public class User {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+	private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String fullname;
+
+    private boolean enabled;
+    
+    @ManyToMany(cascade = {
+    	    CascadeType.PERSIST,
+    	    CascadeType.MERGE
+    	})
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    
+    private Set<Role> roles;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the fullname
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+
+	/**
+	 * @param fullname the fullname to set
+	 */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	/**
+	 * @return the enabled
+	 */
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	/**
+	 * @param enabled the enabled to set
+	 */
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+    
+    
 }
